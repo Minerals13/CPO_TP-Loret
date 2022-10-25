968eb5da4bfb1be1765bfb43c4813ae6804ca379
Emilien Loret <emilien.loret@epfedu.fr>
2022-10-25 12:14:00 +0200

    Création d'un nouveau projet : manip

---
+++ TP2_manip_LORET/src/tp2_manip_loret/TP2_manip_LORET.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_manip_loret;
+
+/**
+ *
+ * @author emili
+ */
+public class TP2_manip_LORET {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        
+        
+        
+        
+    }
+    
+}
