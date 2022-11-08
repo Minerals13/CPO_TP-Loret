9783ef62444b807938493d94cee84d56b0550d5c
Emilien Loret <emilien.loret@epfedu.fr>
2022-11-08 17:18:30 +0100

    Création nouveau projet (héritage entre les classes) Une classe, 2 classe héritante

---
+++ TP3_Relation_LORET/src/tp3_relation_loret/TP3_Relation_LORET.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp3_relation_loret;
+
+/**
+ *
+ * @author emili
+ */
+public class TP3_Relation_LORET {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+        épée Exca = new épée(Excalibur, 7, 5);
+        épée Dura = new épée(Durandal, 4, 7);
+        
+        baton Chên = new baton(Chêne, 4, 7);
+        baton Char = new baton(Charme, 5, 6);
+        
+        
+    }
+    
+}
